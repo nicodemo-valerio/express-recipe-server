910038f7c6da67528534926d2c6e9cd0e4cfdeca
nicodemo <nicodemo@Nicodemos-MacBook-Air.local>
2019-04-06 10:40:13 +0200

    comments

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
+// index.js
 const express = require('express');
 const app = express();
-//const exphbs = require('express-handlebars');
 const recipe = require('./models/recipe');
-
-//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-//app.set('view engine', 'handlebars');
-
-/* app.get('/', (req, res) => {
-    recipe.getRecipes((err, recipes) => {
-        if (err) {
-            throw err;
-        } else {
-            res.render('index',
-                {
-                    title: 'Recipes',
-                    recipes
-                });
-        }
-    });
-}); */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,3 +1,5 @@
+// routes/api/recipes.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
